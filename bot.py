46746ac7042a33189f3e5e2d4b28e2c37a597328
Beatriz Martins <beatrizmartinsrt@gmail.com>
2022-03-26 12:25:19 -0300

    first commit

---
+++ bot.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+pyautogui.PAUSE = 2
+
+
+pyautogui.alert("bot fâ do joao gomes =)")
+pyautogui.press("win")
+pyautogui.write("brave")
+pyautogui.press("enter")
+pyautogui.write("https://www.youtube.com/watch?v=jFLN9lyEHx0")
+pyautogui.press("enter")
